8cf9940378bcf354c567e7f14c4b7662da9bb634
lasoupedjour <lasoupedjour@gmail.com>
2016-08-09 18:24:07 -0500

    c

---
+++ coca/js/cxn1.js
@@ -0,0 +1,31 @@
+var video_out = document.getElementById("vid-box");
+login();
+
+function login() {
+	var phone = window.phone = PHONE({
+		number        : "C1" || "Anonymous", // listen on username line else Anonymous
+		publish_key   : 'pub-c-4c902a56-d0d3-4f28-8424-8d0400870e89', // Your Pub Key
+		subscribe_key : 'sub-c-172d1fbc-5b79-11e6-8ee6-0619f8945a4f', // Your Sub Key
+	});	
+	phone.ready(function(){makeCall();});
+	phone.receive(function(session){
+		session.connected(function(session) { video_out.appendChild(session.video);console.log("esperando video!")});
+		session.ended(function(session) { video_out.innerHTML=''; });
+	});
+	return false;
+}
+
+function makeCall(){
+	if (!window.phone) alert("error en logueo!");
+	else phone.dial("C2");
+	return false;
+}
+
+function errWrap(fxn, form){
+	try {
+		return fxn(form);
+		} catch(err) {
+		alert("WebRTC is currently only supported by Chrome, Opera, and Firefox");
+		return false;
+	}
+}
