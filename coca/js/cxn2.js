d86974d65c3a26a2622c2c5515cefe32a5610f09
lasoupedjour <lasoupedjour@gmail.com>
2016-08-10 00:25:27 -0500

    d

--- coca/js/cxn2.js
+++ coca/js/cxn2.js
@@ -1,6 +1,8 @@
 var video_out = document.getElementById("vid-box");
 var stream;
 login();
+var contReady = 0;
+
 
 function login() {
 	var phone = window.phone = PHONE({
@@ -17,22 +19,42 @@ function login() {
 	stream = PUBNUB.init({
         publish_key: 'pub-c-4c902a56-d0d3-4f28-8424-8d0400870e89',
         subscribe_key: 'sub-c-172d1fbc-5b79-11e6-8ee6-0619f8945a4f',
-		ssl : (('https:' == document.location.protocol) ? true : false)
-    });
+		ssl : (('https:' == document.location.protocol) ? true : false),
+		uuid : 'C2'
+	});
 	
 	
 	stream.subscribe({
 		channel: 'coca',
-		message: function(m){console.log(m)}
-	}); 
+		message: function(m){
+			console.log(m);
+			
+			if(m.accion == 'ready'){
+				contReady ++;
+			}
+			
+			
+			
+		},
+		presence: function(m) {
+			console.log(m);
+		}
+	});
 	
 	return false;
 }
 
 function enviarMensaje(){
-	stream.publish({
+	/*stream.publish({
 		channel: 'coca',
 		message: {"accion":"ready"}
+	}); */
+	stream.here_now({
+	  channel: 'coca',
+	  state: true,
+	  callback: function(msg) {
+		console.log(msg);
+	  }
 	});
 }
 
