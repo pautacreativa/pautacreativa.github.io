23531a03ea88572a0e3f762973b0835252244614
lasoupedjour <lasoupedjour@gmail.com>
2016-08-09 21:06:52 -0500

    c

--- coca/js/cxn1.js
+++ coca/js/cxn1.js
@@ -14,7 +14,7 @@ function login() {
 		session.ended(function(session) { video_out.innerHTML=''; });
 	});
 	
-	stream = PUBNUB.init({
+	/*stream = PUBNUB.init({
         publish_key: 'pub-c-4c902a56-d0d3-4f28-8424-8d0400870e89',
         subscribe_key: 'sub-c-172d1fbc-5b79-11e6-8ee6-0619f8945a4f'
     });
@@ -22,16 +22,16 @@ function login() {
 	stream.subscribe({
 		channel: 'coca',
 		message: function(m){console.log(m)}
-	});
+	});*/
 	
 	return false;
 }
 
 function enviarMensaje(){
-	stream.publish({
+	/*stream.publish({
 		channel: 'coca',
 		message: {"color":"blue"}
-	});
+	});*/
 }
 
 function makeCall(){
